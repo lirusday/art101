bca664ff28139fcca95b6e17e3a6fa5a83dac234
lirusday <114507435+lirusday@users.noreply.github.com>
2022-10-12 19:24:44 -0700

    Lab 4

---
+++ lab4/js/lab.js
@@ -0,0 +1,20 @@
+/**
+ * A minesweeper clone.
+ *
+ * A grid based game where the user attempts to reveal all non-bomb squares but loses if a bomb is revealed.
+ *
+ * @link   lirusday.github.io/art101/lab4
+ * @file   This is a javascript file with the pusedocode for a minesweeper clone.
+ * @author Daniel Shim and Ganzorig Gantushig.
+ * @since  10.12.22
+ */
+
+//Print the grid
+//Check for user's first input on any squares in the grid.
+  //Randomly generate bombs within the grid unselected.
+    //Assign each square a number based on the number of bombs adjacent.
+    //Reveal the user's square and any adjacent blank squares.
+    //Reveal the squares adjacent to black squares.
+//Reveal squares the user clicks on
+  //Reveal any blank squares adjacent to the clicked square and any squares adjacent to them.
+    //End the game whenever a bomb is clicked or there are no hidden squares without a bomb.
