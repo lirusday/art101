edc9bbff63ffe7d386f4b71e9382bea0378d73ea
lirusday <114507435+lirusday@users.noreply.github.com>
2022-10-17 21:05:53 -0700

    Lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,22 @@
+/**
+ * Author: Daniel Shim
+ * Created: 10.17.2022
+ *
+ * (c) Copyright by Daniel Shim.
+**/
+
+// Define Variables
+make = "Volkswagen";
+model = "Beetle";
+color = "Silver";
+year = 2017;
+
+// Calculate
+age = 2022 - year;
+
+// Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
