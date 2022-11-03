1cb3bc0526a0166676f4ca6ae069115303fc0b96
lirusday <114507435+lirusday@users.noreply.github.com>
2022-11-03 04:38:37 -0700

    Lab 10

---
+++ lab10/js/lab.js
@@ -0,0 +1,25 @@
+/**
+ * Author: Daniel Shim
+ * Created: 11.3.2022
+ *
+ * (c) Copyright by Daniel Shim
+ **/
+
+ //Function
+
+ function sortUserName() {
+   var userName = document.getElementById("user-name").value;
+   console.log("userName =", userName);
+   //split string into letters in array
+   var nameArray = userName.split('');
+   console.log("nameArray =", nameArray);
+   //sort array
+   var arraySort = nameArray.sort();
+   console.log("arraySort =", arraySort);
+   //join array
+   var nameJoin = arraySort.join('');
+   console.log("nameJoin =", nameJoin);
+   //return sorted name
+   document.getElementById("output").innerHTML = nameJoin;
+ }
+ 
