7fa6165f83cd9f1716edcae24013fb3a5abaa29a
lirusday <114507435+lirusday@users.noreply.github.com>
2022-11-14 21:57:13 -0800

    Lab 13

---
+++ lab13/js/lab.js
@@ -0,0 +1,29 @@
+/**
+ * Author: Daniel Shim
+ * Created: 11.14.2022
+ *
+ * (c) Copyright by Daniel Shim
+ **/
+
+function fizzBuzz() {
+  for (var num = 0; num < 200; num++) {
+    var out = "";
+    if (num % 3 == 0) {
+      out += "Fizz";
+    }
+    if (num % 5 == 0) {
+      out += "Buzz";
+    }
+    if (num % 7 == 0) {
+      out += "Boom";
+    }
+    if (out != "") {
+      out = " - " + out + "!"
+    }
+    $("#output").append("<p>" + num.toString() + out + "</p>");
+  }
+}
+
+$("#submit").click(function() {
+  fizzBuzz();
+});
