81467640303a3b8994f96dc35fc9cdd0cd0dcf0b
lirusday <114507435+lirusday@users.noreply.github.com>
2022-11-29 10:08:36 -0800

    Lab 16.2

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -5,15 +5,33 @@
  * (c) Copyright by Daniel Shim
  **/
 
-//Functions
+//Variables
 
-$.ajax({
-    url: "https://xkcd.com/info.0.json",
-    type: "GET",
-    data: { get_param: 'value' },
-    dataType: "json",
-    success: function(comicObj) {
-      $("#output").append("<h1>" + comicObj.title + "</h1>");
-      $("#output").append("<img src=\"" + comicObj.img + "\" alt=\""+ comicObj.alt +"\">");
-    }
-})
+var comicObj = {};
+
+//Functions
+function getAndPutData(num) {
+  $.ajax({
+      url: "https://xkcd.com/" + num + "/info.0.json",
+      type: "GET",
+      data: { get_param: 'value' },
+      dataType: "json",
+      success: function(data) {
+        Object.assign(comicObj, data);
+        $("#output").append("<h1>" + comicObj.title + "</h1>");
+        $("#output").append("<img src=\"" + comicObj.img + "\" alt=\""+ comicObj.alt +"\">");
+      }
+  })
+}
+
+$("#prev").click(function() {
+  $("#output").empty();
+  getAndPutData(comicObj.num - 1);
+});
+
+$("#next").click(function() {
+  $("#output").empty();
+  getAndPutData(comicObj.num + 1);
+});
+
+getAndPutData(1);
